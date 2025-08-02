f0a98465acbfd89a29e5b4d586871a55cd327fe2
straigalex <straigalex@myvuw.ac.nz>
2025-08-02 16:21:06 +1200

    Added basic timer-based growth system for saplings. relates to #1.

--- items/sapling.gd
+++ items/sapling.gd
@@ -1,2 +1,23 @@
 class_name Sapling
 extends AnimatableBody3D
+
+var progress:int = 0
+
+const MAX_PROGRESS:int = 4
+
+func plant():
+	$ItemShape.can_be_grabbed = false
+	$Timer.start()
+
+func grow():
+	if progress < MAX_PROGRESS:
+		$Mesh.mesh.size.y += 1
+		$Mesh.position.y += 0.5
+		$Collider.shape.size.y += 1
+		$Collider.position.y += 0.5
+		progress += 1
+	else:
+		$Timer.stop()
+
+func _on_timer_timeout() -> void:
+	grow()
