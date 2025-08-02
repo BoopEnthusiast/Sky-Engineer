9cec784bdac426549c43a7e697565c241c4e08af
straigalex <straigalex@myvuw.ac.nz>
2025-08-02 18:00:17 +1200

    Sapling growth is now much smoother
    Sapling growth is now much smoother, and runs off the _process() function, rather than a timer. 
    Relates to #1

--- items/sapling.gd
+++ items/sapling.gd
@@ -1,23 +1,40 @@
 class_name Sapling
 extends AnimatableBody3D
 
-var progress:int = 0
+@onready var starting_height = $Mesh.mesh.size.y
 
-const MAX_PROGRESS:int = 4
+var progress:float = 0
+
+const MAX_PROGRESS:int = 20
+
+const GROWTH_PER_SEC:float = 1
+
+var growing:bool = false
+
+func _process(delta):
+	if growing:
+		grow_slow(delta)
 
 func plant():
 	$ItemShape.can_be_grabbed = false
-	$Timer.start()
+	growing = true
 
-func grow():
-	if progress < MAX_PROGRESS:
-		$Mesh.mesh.size.y += 1
-		$Mesh.position.y += 0.5
-		$Collider.shape.size.y += 1
-		$Collider.position.y += 0.5
-		progress += 1
-	else:
-		$Timer.stop()
+func reset():
+	progress = 0
+	$Mesh.mesh.size.y = starting_height
+	$Mesh.position.y = 0
+	$Collider.shape.size.y = starting_height
+	$Collider.position.y = 0
 
-func _on_timer_timeout() -> void:
-	grow()
+func grow_slow(time:float):
+	if progress < MAX_PROGRESS and growing:
+		progress += time
+		
+		$Mesh.mesh.size.y = starting_height + (GROWTH_PER_SEC * progress)
+		$Mesh.position.y = (GROWTH_PER_SEC/2) * progress
+		
+		$Collider.shape.size.y = starting_height + (GROWTH_PER_SEC * progress)
+		$Collider.position.y = (GROWTH_PER_SEC/2) * progress
+		
+	elif growing:
+		growing = false
