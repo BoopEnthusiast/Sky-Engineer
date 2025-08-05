22a01521adbc4524398cfece32cd9dac668139d8
straigalex <straigalex@myvuw.ac.nz>
2025-08-05 23:23:49 +1200

    Cleaned up sapling code.
    Removed unnessecery variables, added some comments.

--- items/sapling.gd
+++ items/sapling.gd
@@ -5,7 +5,7 @@ extends AnimatableBody3D
 
 var progress:float = 0
 
-const MAX_PROGRESS:int = 4
+@export var max_progress:int = 4
 
 const GROWTH_PER_SEC:float = 1
 
@@ -13,26 +13,36 @@ var growing:bool = false
 
 var branch = true
 
-var number_of_limbs:int = 1
+var number_of_limbs:int = 2
 
 var number_of_sublimbs:int = 0
 
 var limbs = [0]
 
-
 func _ready():
-	plant()
+	#plant()
 	pass
 
 func _process(delta):
+	#I'm not putting the full grow function in here.
+	#just calling it, and passing in delta allows
+	#for greater flexability.
 	if growing:
 		grow_slow(delta)
 
 func plant():
+	# Stops tree from being grabbed, and sets it growing
+	# TODO integrate with vertex snapping, so the tree
+	#      can be properly planted on surfaces.
+	
 	$ItemShape.can_be_grabbed = false
 	growing = true
 
+# DEPRECATED
 func reset():
+	# This is intended to be used to break the tree back down
+	# if the vertex it's on is destroyed. It doesn't work with
+	# the current tree mesh.
 	progress = 0
 	$Mesh.mesh.size.y = starting_height
 	$Mesh.position.y = 0
@@ -40,22 +50,20 @@ func reset():
 	$Collider.position.y = 0
 
 func grow_slow(time:float):
-	if progress < MAX_PROGRESS and growing:
+	# grows the tree by a certan amount each frame.
+	# TODO randomize branch placement, improve collision detection.
+	if progress < max_progress and growing:
 		progress += time
 		
 		var timegrowth = GROWTH_PER_SEC * time
-		var growth = (GROWTH_PER_SEC * progress)
-		var height = starting_height + growth
 		
-		if(limbs.size() < 3 and progress > 2):
+		if(limbs.size() < number_of_limbs + 1 and progress > 2):
 			limbs.append($Mesh.spawn_limb(0,1,Vector3(0,0,1), 1,0.2))
 			limbs.append($Mesh.spawn_limb(0,1, Vector3(1,0,0), 0.5,0.2))
 			#limbs.remove_at(0)
 		
 		for index in limbs:
 			$Mesh.grow_limb(index,timegrowth)
-
-		
 		
 		$Collider.shape.size.y = starting_height + (GROWTH_PER_SEC * progress)
 		$Collider.position.y = (GROWTH_PER_SEC/2) * progress
--- items/tree_mesh.gd
+++ items/tree_mesh.gd
@@ -1,61 +1,70 @@
 extends MultiMeshInstance3D
 
-var count:int = 0
+var new_limb_index:int = 0
 
 func _ready():
-	var bas = Basis()
-	bas = bas.scaled(Vector3(1,2,1))
-	multimesh.set_instance_transform(0,Transform3D(bas ,Vector3(0,0,0)))
+	var trunk_basis = Basis()
+	trunk_basis = trunk_basis.scaled(Vector3(1,2,1))
+	multimesh.set_instance_transform(0,Transform3D(trunk_basis ,Vector3(0,0,0)))
 
 
-func spawn_limb(parent_index:int,at:float, direction:Vector3, angle:float, width:float):
-	count += 1
-	var parent = multimesh.get_instance_transform(parent_index)
+func spawn_limb(parent_index:int,at:float, direction:Vector3, angle:float, width:float) -> int:
+	# Start rendering a limb instance branching from the limb represented by parent_index. Return the index of the new limb
 	
-	var cord = Vector3(0,at,0)
+	new_limb_index += 1
 	
-	var newloc = parent * cord
+	var parent:Transform3D = multimesh.get_instance_transform(parent_index)
+
+	var limb_origin:Vector3 = parent * Vector3(0,at,0)
 	
-	var bas = parent.basis
+	var new_basis = Basis()
 	
-	var rot = bas.get_rotation_quaternion()
-	var dir = rot.get_axis()
-	var anf = rot.get_angle()
-	var sca = bas.get_scale()
-	var newbas = Basis()
+	#the following snipit of code may be useful if you want to dasy-chain limbs together
+	#var parent_basis:Basis = parent.basis
+	#var rot = parent_basis.get_rotation_quaternion()
+	#var dir = rot.get_axis()
+	#var anf = rot.get_angle()
+	#var sca = parent_basis.get_scale()
+	#new_basis = newbasis.scaled(Vector3(sca.x * 0.35, 1.0, sca.z * 0.35)) 
+	#new_basis = newbasis.rotated(Vector3(0,1,0),0.785)
+	#new_basis = newbasis.rotated(Vector3(parent_direction,parent_angle)
 	
-	#newbas = newbas.scaled(Vector3(sca.x * 0.35, 1.0, sca.z * 0.35))
-	#newbas = newbas.rotated(Vector3(0,1,0),0.785)
-	newbas = newbas.scaled(Vector3(width,0.0001,width))
-	newbas = newbas.rotated(direction,angle)
+	new_basis = new_basis.scaled(Vector3(width,0.0001,width))
+	new_basis = new_basis.rotated(direction,angle)
 	
 	
-	multimesh.set_instance_transform(count,Transform3D(newbas,newloc) )
+	multimesh.set_instance_transform(new_limb_index,Transform3D(new_basis,limb_origin) )
 	
-	var child_origin = multimesh.get_instance_transform(count).origin
+	var child_origin = multimesh.get_instance_transform(new_limb_index).origin
 	
-	return count
-	pass
+	return new_limb_index
 
 func grow_limb(index:int,amount:float):
-	var tra = multimesh.get_instance_transform(index)
-	var bas = tra.basis
-	var sca = bas.get_scale()
-	var qar = bas.get_rotation_quaternion()
-	var dir = qar.get_axis().normalized()
-	var ang = qar.get_angle()
+	# Grows the limb represented by the given index by a given amount
+	# TODO make limbs get thiccer as they grow...
 	
-	var newbas = Basis()
-	newbas = newbas.scaled(Vector3(sca.x,sca.y + amount,sca.z))
-	if dir.is_normalized():
-		newbas = newbas.rotated(dir,ang)
+	# TODO I don't like having to make a new transform every time. I could perhaps store the old scale
+	#      in custom instance data. At least that would mean having to dig around the old transform
+	#      looking for variables less.
 	
-	multimesh.set_instance_transform(index,Transform3D(newbas,tra.origin))
-	pass
+	var old_transform:Transform3D = multimesh.get_instance_transform(index)
+	var old_basis:Basis = old_transform.basis
+	var old_scale:Vector3 = old_basis.get_scale()
+	var rotation_quarternion:Quaternion = old_basis.get_rotation_quaternion()
+	var direction:Vector3 = rotation_quarternion.get_axis().normalized()
+	var angle:float = rotation_quarternion.get_angle()
 	
+	var new_basis:Basis = Basis()
+	new_basis = new_basis.scaled(Vector3(old_scale.x,old_scale.y + amount,old_scale.z))
+	if direction.is_normalized():
+		new_basis = new_basis.rotated(direction,angle)
+	
+	multimesh.set_instance_transform(index,Transform3D(new_basis,old_transform.origin))
 
+# DEPRECATED
 func transform_limb(index:int, height:float, width:float, direction:Vector3, angle:float, location:float):
-	var bas = Basis()
-	bas = bas.scaled(Vector3(width,height,width))
-	bas = bas.rotated(direction,angle)
-	multimesh.set_instance_transform(index,Transform3D(bas ,Vector3(0,location,0)))
+	# provides very specific control of limb features. Evil.
+	var new_basis = Basis()
+	new_basis = new_basis.scaled(Vector3(width,height,width))
+	new_basis = new_basis.rotated(direction,angle)
+	multimesh.set_instance_transform(index,Transform3D(new_basis ,Vector3(0,location,0)))
