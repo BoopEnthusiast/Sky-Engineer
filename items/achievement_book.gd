748523c07fa44b4925fecce88c6ba7fd287fb2a8
baolong2005 <135151094+baolong2005@users.noreply.github.com>
2025-08-11 12:44:31 +1200

    Flipping pages code
    Adjustment to the achievement

--- items/achievement_book.gd
+++ items/achievement_book.gd
@@ -1,12 +1,48 @@
 extends AnimatableBody3D
 
 @onready var animation_player = $AnimationPlayer
+@onready var page_container = $PageContainer
+@onready var page_flip_button_right = $RightCover/PageFlipButtonRight
+@onready var page_flip_button_left = $"Left Cover/PageFlipButtonLeft"
 
+var is_in_inventory = true
+var is_open = true
 
+func _ready() -> void:
+	page_container.visible = false # Hide the pages when not being opened
+	set_book_state_from_inventory() 
+	
+# Flipping pages 
+	if page_flip_button_left:
+		page_flip_button_left.input_event.connect(_on_page_flip_button_left_input_event)
+	if page_flip_button_right:
+		page_flip_button_right.input_event.connect(_on_page_flip_button_right_input_event)
+	
+func set_book_state_from_inventory():
+	if is_in_inventory:
+		_on_item_shape_being_put_into_inventory()
+	else:
+		_on_item_shape_being_taken_from_inventory()
 
 func _on_item_shape_being_put_into_inventory() -> void:
 	animation_player.play("Close")
+	page_container.visible = false 
+	print("Book is in inventory")
 
 
 func _on_item_shape_being_taken_from_inventory() -> void:
 	animation_player.play("Open")
+	page_container.visible = true
+	print("Book is out of inventory")
+
+
+func _on_page_flip_button_right_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		if is_open: # Only allow page flipping if the book is open
+			page_container.flip_page_backward()
+
+
+func _on_page_flip_button_left_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		if is_open: # Only allow page flipping if the book is open
+			page_container.flip_page_forward()
