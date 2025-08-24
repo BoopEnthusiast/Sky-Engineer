d8fff81a98c59a7502a5780358c2caa989e50d43
Boop <102631339+BoopEnthusiast@users.noreply.github.com>
2025-08-25 02:09:44 +1200

    Get main menu 3d selection working

--- interactibles/menu/main_menu/button_3d.gd
+++ interactibles/menu/main_menu/button_3d.gd
@@ -21,7 +21,10 @@ func _process(delta: float) -> void:
 	# Move towards mouse
 	if _is_mouse_inside:
 		var weight: float = 1 - exp(-LERP_SPEED * delta)
-		counterpart_in_2d.global_position = counterpart_in_2d.global_position.lerp(_last_mouse_position, weight)
+		var position_to_move_to := _last_mouse_position - counterpart_in_2d.size / 2
+		var distance_to_move := minf(position_to_move_to.length() * 0.1, position_to_move_to.distance_to(counterpart_in_2d.global_position))
+		position_to_move_to = -position_to_move_to.direction_to(counterpart_in_2d.global_position) * distance_to_move  + _initial_position
+		counterpart_in_2d.global_position = counterpart_in_2d.global_position.lerp(position_to_move_to, weight)
 	# Move back to start
 	elif _initial_position != counterpart_in_2d.position:
 		var weight: float = 1 - exp(-LERP_SPEED * delta)
@@ -32,7 +35,7 @@ func _process(delta: float) -> void:
 
 func _on_static_body_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
 	if event is InputEventMouseMotion:
-		_last_mouse_position = get_viewport().get_camera_3d().unproject_position(event_position)
+		_last_mouse_position = camera.unproject_position(event_position)
 
 
 func _on_static_body_mouse_entered() -> void:
