451c0cd343ec6120d7bdaedb1a8c3d355cca4aa8
Boop <102631339+BoopEnthusiast@users.noreply.github.com>
2025-08-26 00:14:46 +1200

    Buttons look pressed or hovered over

--- interactibles/menu/main_menu/button_3d.gd
+++ interactibles/menu/main_menu/button_3d.gd
@@ -3,8 +3,6 @@ extends ProjectedMenuItem
 
 
 signal pressed()
-signal stopped_pressing()
-signal released()
 
 const LERP_SPEED = 4.0
 
@@ -13,6 +11,8 @@ var is_pressed := false
 var _is_mouse_inside := false
 var _initial_position: Vector2
 var _last_mouse_position: Vector2
+
+@onready var _mesh: MeshInstance3D = $StaticBody/Mesh
 
 
 func _ready() -> void:
@@ -39,31 +39,49 @@ func _process(delta: float) -> void:
 	super(delta)
 
 
-func _on_static_body_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
-	if event is InputEventMouseMotion:
-		_last_mouse_position = camera.unproject_position(event_position)
-	elif event.is_action_pressed("select") and not is_pressed:
-		is_pressed = true
-		pressed.emit()
-	elif event.is_action_released("select") and is_pressed:
-		is_pressed = true
-		released.emit()
-
-
 func _input(event: InputEvent) -> void:
 	if event.is_action_released("select") and is_pressed:
 		await get_tree().process_frame
 		if is_pressed:
 			is_pressed = false
-			stopped_pressing.emit()
+			_on_let_go()
+
+
+func _on_static_body_input_event(camera: Node, event: InputEvent, event_position: Vector3, _normal: Vector3, _shape_idx: int) -> void:
+	if event is InputEventMouseMotion:
+		_last_mouse_position = camera.unproject_position(event_position)
+	elif event.is_action_pressed("select") and not is_pressed:
+		is_pressed = true
+		_on_button_pressed()
+	elif event.is_action_released("select") and is_pressed:
+		is_pressed = true
+		_on_let_go()
+		pressed.emit()
+
+
+func _on_hovered_over() -> void:
+	_mesh.mesh.material.albedo_color = Color.LIGHT_GRAY
+
+
+func _on_button_pressed() -> void:
+	_mesh.mesh.material.albedo_color = Color.DARK_GRAY
+
+
+func _on_let_go() -> void:
+	if _is_mouse_inside:
+		_on_hovered_over()
+	elif not is_pressed:
+		_mesh.mesh.material.albedo_color = Color.WHITE
 
 
 func _on_static_body_mouse_entered() -> void:
 	_is_mouse_inside = true
+	_on_hovered_over()
 
 
 func _on_static_body_mouse_exited() -> void:
 	_is_mouse_inside = false
+	_on_let_go()
 
 
 func _on_viewport_size_changed() -> void:
--- interactibles/menu/main_menu/button_3d_tool.gd
+++ interactibles/menu/main_menu/button_3d_tool.gd
@@ -20,15 +20,14 @@ extends Button3D
 		_text_mesh.position.z = value.z / 2 + _text_mesh.mesh.depth / 2
 
 
-@onready var _mesh: MeshInstance3D = $StaticBody/Mesh
 @onready var _collider: CollisionShape3D = $StaticBody/Collider
 @onready var _text_mesh: MeshInstance3D = $StaticBody/TextMesh
 
 
 func _ready() -> void:
-	_mesh.mesh = _mesh.mesh.duplicate(true)
 	_collider.shape = _collider.shape.duplicate(true)
 	_text_mesh.mesh = _text_mesh.mesh.duplicate(true)
+	_mesh.mesh = _mesh.mesh.duplicate(true)
 	
 	if not Engine.is_editor_hint():
 		super()
