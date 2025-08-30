e9ec080d72764b74c5d0fabae05dec847b35364e
Boop <102631339+BoopEnthusiast@users.noreply.github.com>
2025-08-30 18:52:10 +1200

    Start of craft area and base workbench

--- globals/recipes.gd
+++ globals/recipes.gd
@@ -1,6 +1,13 @@
 extends Node
 
 
-const INVENTORY: Dictionary[Array, PackedScene] = { # List of classes (in string form) that are required in the recipes you can make in the inventory, then the preload of the item 
-	
+## Dictionary, with the key of the StringName group name that are required in the recipes you can make in the inventory, and the value of how many of nodes with that group is required.[br]
+## Then the preload of the item as the value of that initial dictionary. 
+const INVENTORY: Dictionary[Dictionary, PackedScene] = { 
+	{ &"Wood": 5, }: preload("res://items/workbench/base_workbench.tscn"),
+}
+
+## Same as [member INVENTORY].
+const BASE_WORKBENCH: Dictionary[Dictionary, PackedScene] = {
+	{ &"Wood": 7, }: preload("res://items/cauldron/cauldron.tscn"),
 }
---
+++ items/workbench/base_workbench.gd
@@ -0,0 +1,10 @@
+class_name BaseWorkbench
+extends AnimatableBody3D
+
+
+func _on_node_entered(_node: Node3D) -> void:
+	pass
+
+
+func _on_node_exited(_node: Node3D) -> void:
+	pass
