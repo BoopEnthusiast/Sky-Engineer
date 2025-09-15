56a0527364405af001a37b951042280b5615698a
Boop <102631339+BoopEnthusiast@users.noreply.github.com>
2025-09-15 14:48:04 +1200

    basic workbench example

--- items/workbench/base_workbench.gd
+++ items/workbench/base_workbench.gd
@@ -2,9 +2,8 @@ class_name BaseWorkbench
 extends AnimatableBody3D
 
 
-func _on_node_entered(_node: Node3D) -> void:
-	pass
+@onready var craft_area: CraftArea = $CraftArea
 
 
-func _on_node_exited(_node: Node3D) -> void:
-	pass
+func _ready() -> void:
+	craft_area.add_recipe_list(Recipes.BASE_WORKBENCH)
