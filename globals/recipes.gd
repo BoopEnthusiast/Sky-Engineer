8ade821b580f6418b0f5cdc455ddc3740957b547
baolong2005 <135151094+baolong2005@users.noreply.github.com>
2025-09-19 19:06:58 +1200

    Adjust the code of the wooden workbench

--- globals/recipes.gd
+++ globals/recipes.gd
@@ -4,10 +4,14 @@ extends Node
 ## Dictionary, with the key of the StringName group name that are required in the recipes you can make in the inventory, and the value of how many of nodes with that group is required.[br]
 ## Then the preload of the item as the value of that initial dictionary. 
 const INVENTORY: Dictionary[Dictionary, PackedScene] = { 
-	{ &"Wood": 5, }: preload("res://items/workbench/base_workbench.tscn"),
+	{ &"Wood": 5, }: preload("res://items/workbench/wooden_workbench.tscn"),
 }
 
 ## Same as [member INVENTORY].
 const BASE_WORKBENCH: Dictionary[Dictionary, PackedScene] = {
+	{ &"Wood": 7, }: preload("res://items/cauldron/cauldron.tscn"),
+}
+## Same as [member INVENTORY].
+const WOODEN_WORKBENCH: Dictionary[Dictionary, PackedScene] = {
 	{ &"Wood": 7, }: preload("res://items/cauldron/cauldron.tscn"),
 }
---
+++ items/workbench/wooden_workbench.gd
@@ -0,0 +1,9 @@
+class_name BaseWorkbench
+extends AnimatableBody3D
+
+
+@onready var craft_area: CraftArea = $CraftArea
+
+
+func _ready() -> void:
+	craft_area.add_recipe_list(Recipes.WOODEN_WORKBENCH)
