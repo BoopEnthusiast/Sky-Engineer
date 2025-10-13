885ef9a2fd418198e1d415334e4a42721af57c1e
baolong2005 <135151094+baolong2005@users.noreply.github.com>
2025-10-13 16:10:28 +1300

    Sieve attempt (fail, code already removed)

---
+++ items/sieve/sieve.gd
@@ -0,0 +1,6 @@
+class_name Sieve
+extends AnimatableBody3D
+
+@onready var collider: CollisionShape3D = $Collider
+@onready var mesh: MeshInstance3D = $Mesh
+@onready var dust: GPUParticles3D = $Dust
